b4d89801dca27558888c0db0e44f8d94e6282121
m-ert <mertekincmp@gmail.com>
2024-09-26 01:21:59 +0300

    established MongoDB connection, no data yet

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from './config/db.js'
 
 const app = express();
+dotenv.config();
+
+
+app.get("/products", (req,res) => {
+    res.send("productss");
+})
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 })
+
