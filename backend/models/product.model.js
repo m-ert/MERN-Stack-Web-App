1ebf4a053b2d2dbff0395b03a330b332298a1b9d
m-ert <mertekincmp@gmail.com>
2024-09-28 01:25:25 +0300

    added model and post api

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -12,7 +12,10 @@ const productSchema = new mongoose.Schema({
     image:{
         type: String,
         required: true
-},} {
+},
+},
+
+{
     timestamps: true // createdAt updatedAt
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from './config/db.js'
+import {connectDB} from './config/db.js';
+import Product from "./models/product.model.js";
 
 const app = express();
 dotenv.config();
 
+app.use(express.json());    // allows us to accept JSON data in the req.body
 
-app.get("/products", (req,res) => {
-    res.send("productss");
+app.post("/api/products", async (req,res) => {
+    const product = req.body;   //  submitted entry
+    
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({ success: false, message: "Please provide all fields"})
+}
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct});
+}   catch(error){
+    console.error("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error"});
+}
 })
 
 app.listen(5000, () => {
