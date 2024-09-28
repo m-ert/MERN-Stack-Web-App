16f6781c5095a37d6ce6e122452234e3e24a291e
m-ert <mertekincmp@gmail.com>
2024-09-28 18:22:39 +0300

    added get and put controllers

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,40 @@ import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from './config/db.js';
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());    // allows us to accept JSON data in the req.body
+
+app.get("/api/products", async (req, res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json({success: true, data: products});
+} catch (error){
+        console.log("error in fetching products: ", error.message);
+        res.status(500).json({success: false, message: "Server error"});
+}
+});
+
+app.put("/api/products/:id", async (req, res) => {
+
+    const {id} = req.params;
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message: "Invalid product Id"});
+    }
+
+    try{
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({success: true, data: updatedProduct});
+} catch (error){
+        console.log("error in fetching products: ", error.message);
+        res.status(500).json({success: false, message: "Server error"});
+}
+});
 
 app.post("/api/products", async (req,res) => {
     const product = req.body;   //  submitted entry
