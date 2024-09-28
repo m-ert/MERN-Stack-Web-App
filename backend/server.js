e0891b8498fbc4cf017f6b0de6ab038261c729da
m-ert <mertekincmp@gmail.com>
2024-09-28 17:51:56 +0300

    added delete method

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,26 @@ app.post("/api/products", async (req,res) => {
     console.error("Error in Create product:", error.message);
     res.status(500).json({ success: false, message: "Server Error"});
 }
-})
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+    const {id} = req.params;
+
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product deleted"});
+} catch (error){
+        res.status(404).json({success: false, message: "Product not found"});
+}
+});
 
 app.listen(5000, () => {
     connectDB();
     console.log("Server started at http://localhost:5000");
 })
+
+
+
+
+
 
